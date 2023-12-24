19ef85f1bef7791107fde2af66a645431700a3dd
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-12-24 19:48:02 +1100

    Update get_and_post.py

--- get_and_post.py
+++ get_and_post.py
@@ -6,15 +6,19 @@ client = OpenAI(
   api_key = os.environ.get("OPENAI_API_KEY")
 )
 
+role = "You are an author of childrens stories"
 prompt = "Tell me a story about four vikings in valheim. Their names are Harrison Fjord, Aslaug, Skagi Skítráðr and Vikingman. make the story about something that has happened today, in another year. Don't introduce the story, just start telling it."
 
-def get_ai_response(prompt):
+def get_ai_response(role, prompt):
   try:
       response = client.completions.create(
           model="curie",
-          prompt=prompt,
+          messages=[
+             {"role": "system", "content": role},
+             {"role": "user", "content": prompt}
+          ]
       )
-      return response.choices[0].text.strip()
+      return response.choices[0].message.strip()
   except Exception as e:
       print(f"Error: {e}")
 
@@ -32,4 +36,4 @@ def post_to_discord(message):
   except Exception as e:
     print(f"Error: {e}")
 
-post_to_discord(get_ai_response(prompt))
+post_to_discord(get_ai_response(role,prompt))
