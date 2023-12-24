61927fa357b052ee8e1b73e513f814a5805add20
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-12-24 21:01:10 +1100

    Update get_and_post.py

--- get_and_post.py
+++ get_and_post.py
@@ -6,8 +6,8 @@ client = OpenAI(
   api_key = os.environ.get("OPENAI_API_KEY")
 )
 
-prompt = "Just say Hi"
-#prompt = "Tell me a story in less than 200 words about four vikings in valheim. Their names are Harrison Fjord, Aslaug, Skagi Skítráðr and Vikingman. make the story about something that has happened today, in another year. Don't introduce the story, just start telling it."
+#prompt = "Just say Hi"
+prompt = "Tell me a story in less than 200 words about four vikings in valheim. Their names are Harrison Fjord, Aslaug, Skagi Skítráðr and Vikingman. make the story about something that has happened today, in another year. Don't introduce the story, just start telling it."
 
 def get_ai_response(prompt):
   try:
