14db6d63583177656d0e900b96c438ca058a5e65
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-12-24 19:27:39 +1100

    Update get_and_post.py

--- get_and_post.py
+++ get_and_post.py
@@ -3,7 +3,7 @@ import requests
 import os
 
 client = OpenAI(
-  api_key = os.environ["OPENAI_API_KEY"]
+  api_key = os.environ.get("OPENAI_API_KEY")
 )
 
 prompt = "Tell me a story about four vikings in valheim. Their names are Harrison Fjord, Aslaug, Skagi Skítráðr and Vikingman. make the story about something that has happened today, in another year. Don't introduce the story, just start telling it."
@@ -24,7 +24,7 @@ def post_to_discord(message):
       "Content-Type": "application/json"
     }
     response = requests.post(
-      url=os.environ['DISCORD_WEBHOOK_URL'],
+      url=os.environ.get('DISCORD_WEBHOOK_URL'),
       headers=headers,
         json=message
       )
