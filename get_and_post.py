69c8acbec7f8688a22a26190c75de5b1079038b1
MaximumPigs <98137611+MaximumPigs@users.noreply.github.com>
2023-12-24 19:25:27 +1100

    Update get_and_post.py

--- get_and_post.py
+++ get_and_post.py
@@ -1,17 +1,18 @@
-import openai
+from openai import OpenAI
 import requests
 import os
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+client = OpenAI(
+  api_key = os.environ["OPENAI_API_KEY"]
+)
 
 prompt = "Tell me a story about four vikings in valheim. Their names are Harrison Fjord, Aslaug, Skagi Skítráðr and Vikingman. make the story about something that has happened today, in another year. Don't introduce the story, just start telling it."
 
 def get_ai_response(prompt):
   try:
-      response = openai.Completion.create(
-          engine="text-davinci-003",
+      response = client.completions.create(
+          model="gpt-4",
           prompt=prompt,
-          max_tokens=150
       )
       return response.choices[0].text.strip()
   except Exception as e:
